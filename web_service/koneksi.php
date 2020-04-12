7b1e8134715cf0307a3cb8798a8590a957e33cda
isneverdead <isneverdead@github.com>
2020-04-12 11:50:06 +0700

    first commit

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12777779_root2');
+ define('PASS','F@HSO_pbO3L?\Mmc');
+ define('DB','id12777779_restoran');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
